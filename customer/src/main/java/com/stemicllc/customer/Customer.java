ef1fd9a72ef8b10582e8090573c3d170dde6d9cc
nyarunda <ezranyagaka96@gmail.com>
2022-06-09 04:31:07 +0300

    first commit

---
+++ customer/src/main/java/com/stemicllc/customer/Customer.java
@@ -0,0 +1,16 @@
+package com.stemicllc.customer;
+
+import lombok.Builder;
+import lombok.Data;
+
+@Data
+@Builder
+public class Customer {
+
+    private Integer id;
+    private String firstname;
+    private String lastname;
+    private String email;
+    private String phoneno;
+
+}
